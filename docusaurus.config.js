871ab2ef1272fd775fd067c804e701b8dd9f869e
LWanTao <lwantaoo@qq.com>
2023-08-05 01:56:44 +0800

    fix: modify editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/lwantao/l10n4zh-cookbook/',
+            'https://github.com/lwantao/l10n4zh-cookbook/blob/master/',
         },
         blog: {
           showReadingTime: true,
